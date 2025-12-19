dd76d47b28251220fc8dde39c511137fe88b8f61
NARENDRA-KBM <narendra@kbmresorts.com>
2025-12-19 18:33:03 +0530

    Fixed the url

--- src/app/constants/project-constants.ts
+++ src/app/constants/project-constants.ts
@@ -91,7 +91,7 @@ export const projectConstantsLocal = {
     { label: 'No', value: 'No' },
   ],
   // BASE_URL: 'http://localhost:5003/',
-  BASE_URL: 'https://rest.thefintalk.in:5003/',
-  // BASE_URL: 'https://rest.thefintalk.in/hrms/',
+  // BASE_URL: 'https://rest.thefintalk.in:5003/',
+  BASE_URL: 'https://rest.thefintalk.in/hrms/',
   VERSION_DESKTOP: '0.0.0',
 };
